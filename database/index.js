c206c01b61c56c873304c10057ba4f02ae72db93
Ap0sentada <70923059+ap0sentada@users.noreply.github.com>
2021-12-13 22:57:23 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const data = require('./model');
 var _baseValues_ = {};
 let body_ = {};
 body_.Connect = function(i) {
-    if (!i || typeof i != "object" || !i.url) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"faltando 'url' em .create({ url:... })".bold.red}`);
+    if (!i || typeof i != "object" || !i.url) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"faltando 'url' em .create({ url:... })".bold.red}`);
     let db_ = mongoose.connect(i.url, {
         useNewUrlParser: false,
         useUnifiedTopology: true
@@ -13,24 +13,24 @@ body_.Connect = function(i) {
     if (i.response) response = i.response;
     db_.then(() => console.log("[" + "database".yellow.bold + "] " + response)).catch(err => console.log(err));
     this.Variables = function(bases) {
-        if (typeof bases != "object") throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"tipo invalido nas variaveis. crie um corpo de variaveis!".bold.red}`);
+        if (typeof bases != "object") throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"tipo invalido nas variaveis. crie um corpo de variaveis!".bold.red}`);
         _baseValues_ = bases;
     };
     this.Get = async function(key_, id) {
-        if (!key_) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"faltando 'key' em get()".bold.red}`);
-        if (!id) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"faltando 'id' em get()".bold.red}`);
+        if (!key_) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"faltando 'key' em get()".bold.red}`);
+        if (!id) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"faltando 'id' em get()".bold.red}`);
         let user = await data.findOne({ key: `${key_}_${id}` });
         let key__ = _baseValues_[key_];
-        if (!key__) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"você não definiu ".bold.red + key_.bold.yellow + " em .Variables({...})".bold.red}`);
+        if (!key__) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"você não definiu ".bold.red + key_.bold.yellow + " em .Variables({...})".bold.red}`);
         if (!user) {
             user = key__;
         } else user = user.value;
         return Number(user) === NaN ? user : Number(user);
     };
     this.Set = async function(key_, user_, value) {
-        if (!key_) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"faltando 'key' em .set({...})".bold.red}`);
-        if (!_baseValues_[key_]) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"você não definiu ".bold.red + key_.bold.yellow + " em .Variables({...})".bold.red}`);
-        if (!user_) throw TypeError(`${"[".bold}${"Database Error".red.bold}${"]".bold} ${"faltando 'id' em .set({...})".bold.red}`);
+        if (!key_) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"faltando 'key' em .set({...})".bold.red}`);
+        if (!_baseValues_[key_]) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"você não definiu ".bold.red + key_.bold.yellow + " em .Variables({...})".bold.red}`);
+        if (!user_) throw TypeError(`${"[".bold}${"database".yellow.bold }${"]".bold} ${"faltando 'id' em .set({...})".bold.red}`);
         let user = await data.findOne({ key: `${key_}_${user_}` });
         if (!user) {
             const newUser = await data.create({
